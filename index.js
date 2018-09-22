eaaf534e0a9c386abf7126eb9156f4a688b921a6
Fernando Magrosoto V <fmagrosoto@gmail.com>
2018-09-22 17:00:37 -0500

    Prueba con layout e include de Pug

--- index.js
+++ index.js
@@ -27,6 +27,23 @@ app.get('/inventario/:producto/:modelo', function (req, res) {
   res.send(mensaje)
 })
 
+// Primer ejercicio con plantillas
+app.get('/mi-primer-plantilla', function (req, res) {
+  /**
+   * La idea es convertir una página .pug en la plantilla principal
+   * y en esa misma página incrustar las demás vistas.
+   * OJO, lo que se renderiza es, precisamente, la página incrustada y
+   * no la plantilla original.
+   * Revisa el código de estas páginas para más referencia acerca de (include)
+   */
+  let misVariables = {
+    'listado': ['Este es un primer texto',
+      'Lo que estás viendo es un array',
+      'Que se convirtió en variable y se está recorriendo en un include']
+  }
+  res.render('_seccion_xInclude', misVariables)
+})
+
 // Inicializar el servidor
 app.listen(PORT, function () {
   console.log(`Servidor corriendo en el puerto: ${PORT}`)
